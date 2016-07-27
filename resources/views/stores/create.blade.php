6d8c6377ed844677ccb82274236359bc8b4a1362
lexville <alex.kangethe@andela.com>
2016-07-27 12:55:54 +0300

    (Feature) Add create new store form

--- resources/views/stores/create.blade.php
+++ resources/views/stores/create.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h2>Add new store</h2>
+        <h2>Add New Store</h2>
+        {!! Form::open(array('route' => 'stores.create', 'method' => 'POST')) !!}
+
+        <div class="form-group">
+            {!! Form::label('store_name', 'Store Name:', ['class' => 'control-label']) !!}
+            {!! Form::text('store_name', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('store_description', 'Store Description:', ['class' => 'control-label']) !!}
+            {!! Form::textarea('store_description', null, ['class' => 'form-control']) !!}
+        </div>
+        {!! Form::submit('Create Store', ['class' => 'btn btn-primary']) !!}
+        {!! Form::close() !!}
     </div>
 @stop
