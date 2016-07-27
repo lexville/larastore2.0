85fae185a5bad559459faa0e3dab92af267101e6
lexville <alex.kangethe@andela.com>
2016-07-27 16:00:07 +0300

    (Feature) Add user create product test

---
+++ tests/features/UserCanCreateProductForAStore.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Store;
+
+class UserCanCreateProductForAStore extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCanCreateProductForAStore()
+    {
+        $user = factory(User::class)->create();
+
+        $store = factory(Store::class)->create([
+            'store_name' => 'New store',
+            'store_description' => 'New store description found here',
+            'user_id' => 1,
+        ]);
+
+        $this->actingAs($user)
+            ->visit('/store/1/product/create')
+            ->type('New product', 'product_name')
+            ->type('New product description found here', 'product_description')
+            ->press('Create product')
+            ->seeInDatabase('products', ['product_name' => 'New product']);
+    }
+}
