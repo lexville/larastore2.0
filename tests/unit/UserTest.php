9a991bba4917b787904f8bce9dfeff3fbb0704bc
lexville <alex.kangethe@andela.com>
2016-07-27 16:48:04 +0300

    (Feature) Add unit test

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Store;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testStoreCanBeFoundByStoreName()
+    {
+        $createdStore = factory(Store::class)->create([
+            'store_name' => 'New store name',
+            'store_description' => 'New store description found here',
+            'user_id' => 1
+        ]);
+
+        $foundStore = Store::findByStoreName('New store name');
+
+        $this->assertEquals($createdStore->store_name, $foundStore->name);
+    }
+}
