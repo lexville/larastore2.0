42ad2bbfc0c522c8d0a733945447bbd78ed7b598
lexville <alex.kangethe@andela.com>
2016-07-27 15:13:22 +0300

    (Chore) Add ability to show errors

--- resources/views/stores/create.blade.php
+++ resources/views/stores/create.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
+        @if (count($errors))
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <h2>Add New Store</h2>
         {!! Form::open(array('route' => 'stores.create', 'method' => 'POST')) !!}
 
