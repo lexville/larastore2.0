6ec1b1c25b15b07638e0ffec83d56827ea611c4a
lexville <alex.kangethe@andela.com>
2016-07-27 16:16:22 +0300

    (Feature) Add products model and migration

---
+++ app/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['product_name', 'product_description'];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+}
