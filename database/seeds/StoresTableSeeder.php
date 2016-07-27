a92e9c195342af97a7444cf114032b5de88dec1c
lexville <alex.kangethe@andela.com>
2016-07-27 12:58:41 +0300

    (Feature) Add store table seeder

--- database/seeds/StoresTableSeeder.php
+++ database/seeds/StoresTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Store;
 
-class DatabaseSeeder extends Seeder
+class StoresTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(StoresTableSeeder::class);
+        factory(Store::class, 30)->create();
     }
 }
