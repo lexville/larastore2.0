60fe453d8f7a3b7d7f042c6c0bd7e2120ae11ae8
lexville <alex.kangethe@andela.com>
2016-07-28 00:43:50 +0300

    Add section if no products are available

--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -7,22 +7,29 @@
                 Add new product
             </a>
         @endif
-        <table class="table table-hover">
-            <thead>
-              <tr>
-                <th>Product Name</th>
-                <th>Product Description</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach($allProducts as $product)
-                <tr>
-                    <td><a href="#">{{ $product->product_name }}</a></td>
-                    <td>{{ $product->product_description}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-         </table>
-         {{ $allProducts->render() }}
+        @if(count($allProducts) > 0)
+            <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Product Name</th>
+                    <th>Product Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach($allProducts as $product)
+                    <tr>
+                        <td><a href="#">{{ $product->product_name }}</a></td>
+                        <td>{{ $product->product_description}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+             </table>
+             {{ $allProducts->render() }}
+        @else
     </div>
+    <div class="well text-center" style="margin: 50px;">
+        Sorry you this store has no products
+    </div>
+    @endif
+
 @stop
