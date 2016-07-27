065392263350b4adc963af4fd96a350868179dd0
lexville <alex.kangethe@andela.com>
2016-07-27 09:21:48 +0300

    (Feature) Add user tegistration test

---
+++ tests/features/UserRegistrationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserRegistrationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCanRegister()
+    {
+        $this->visit('/register')
+            ->typre('lexville', 'name')
+            ->type('lex@email.com', 'email')
+            ->type('secret', 'password')
+            ->type('secret', 'password_confirmation')
+            ->press('Register')
+            ->seeInDatabase('users', ['name' => 'lexville']);
+    }
+
+    public function testRegisteredUsersNumberIncreases()
+    {
+        factory(User::class, 10)->create();
+
+        $allUsers = User::count();
+
+        $this->count(10, $allUsers);
+    }
+}
