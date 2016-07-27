cef78eb3e199c695faf65df6849e7eeee5177917
lexville <alex.kangethe@andela.com>
2016-07-27 21:11:00 +0300

    (Feature) Add product creation form

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        @if (count($errors))
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <h2>Add New Product</h2>
+        {!! Form::open(array()) !!}
+
+        <div class="form-group">
+            {!! Form::label('product_name', 'Product Name:', ['class' => 'control-label']) !!}
+            {!! Form::text('product_name', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('product_description', 'Product Description:', ['class' => 'control-label']) !!}
+            {!! Form::textarea('product_description', null, ['class' => 'form-control']) !!}
+        </div>
+        {!! Form::submit('Create product', ['class' => 'btn btn-primary']) !!}
+        {!! Form::close() !!}
+    </div>
+@stop
