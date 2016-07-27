e10fb8c612dfd52f9526b08770b0700fd3a21d27
lexville <alex.kangethe@andela.com>
2016-07-27 12:38:23 +0300

    (Feature) Add stores controller

---
+++ app/Http/Controllers/StoresController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Store;
+use App\Http\Requests;
+
+class StoresController extends Controller
+{
+    /**
+     * Method to show the create view
+     *
+     * @return Store create view
+     */
+    public function show()
+    {
+        return view('stores.create');
+    }
+
+    /**
+     * Method to save the information given
+     *
+     * @return redirect to root page
+     */
+    public function store(Request $request)
+    {
+        $newStore = new Store;
+        $newStore->store_name = $request->input('store_name');
+        $newStore->store_description = $request->input('store_description');
+        $request->user()->stores()->save($newStore);
+
+        return redirect('/');
+    }
+}
