430629dc9b26bdc8972642ae63f911f7fc45b687
lexville <alex.kangethe@andela.com>
2016-07-27 09:25:38 +0300

    (Feature) Add user can login test

---
+++ tests/features/UserLoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserLoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCanLogin()
+    {
+        factory(User::class)->create([
+            'name' => 'lexville',
+            'email' => 'lex@email.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $this->visit('/register')
+            ->type('lex@email.com', 'email')
+            ->type('secret', 'password')
+            ->press('Login')
+            ->see('lexville');
+    }
+}
