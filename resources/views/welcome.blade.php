7750e37495b68b5cdf23241c00cd062fcf495856
lexville <alex.kangethe@andela.com>
2016-07-27 12:42:48 +0300

    (Chore) Add href to add new store anchor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     @if(auth()->check())
-        <a href="#" class="btn btn-primary pull-right">Add new store</a>
+        <a href="/store/create" class="btn btn-primary pull-right">Add new store</a>
     @endif
 </div>
 @endsection
